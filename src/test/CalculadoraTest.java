3c617fe9f3ca61c2d87abb03e363be8030af0e66
usuario <usuario@pc20-018>
2023-10-10 10:17:43 +0200

    Paso 1, crear TDD

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pkg.Calculadora;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSuma() {
+		assertEquals(5, Calculadora.suma(2,3));
+	}
+	
+	void testResta() {
+		assertEquals(-1, Calculadora.resta(2,3));
+	}
+
+}
